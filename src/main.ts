6afb0a4a4d6b6cb5a16f2840eb73e8a85b4c6944
DALM1-TT <dalm1-tt@Mac-mini-de-DALM1-TT.local>
2026-03-30 20:28:26 +0200

    fix(auth): demo otp + align entities with migrations

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { Logger, VersioningType } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { ConfigService } from '@nestjs/config';
+import type { Request, Response } from 'express';
 import { AppModule } from './modules/app/app.module';
 import { createSwaggerDocumentation } from './swagger';
 import { LoggingInterceptor } from './interceptors';
@@ -24,6 +25,11 @@ async function bootstrap() {
 	createSwaggerDocumentation(app, port, configService, globalPrefix);
 
 	app.useGlobalInterceptors(new LoggingInterceptor());
+
+	const expressApp = app.getHttpAdapter().getInstance();
+	expressApp.get('/metrics', (_req: Request, res: Response) => {
+		res.type('text/plain').send('');
+	});
 
 	await app.listen(port);
 
